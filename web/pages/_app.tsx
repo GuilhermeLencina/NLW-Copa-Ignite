c953b063b87f1ed8c2e5a7d84be0f5e8e7773980
Guilherme <GuiLencinaP@outlook.com>
2022-11-05 17:42:14 -0300

    Aula 02: Criando rotas e acessos

--- web/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-/*export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}*/
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
